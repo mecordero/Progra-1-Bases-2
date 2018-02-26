7bf84ea92da5b44c17c70075b17b410703d17182
Meli Cordero <melisacorderoarias@hotmail.com>
2018-02-25 18:26:41 -0600

    Con 2 pantallas de interfaz
    Tiene la pantalla de inicio y la pantalla de crear usuario. Lo siguiente es hacer más pantallas o ya hacer cambios en la base de datos con esas 2 pantallas

--- Proyecto 1/app_server/controllers/registro.js
+++ Proyecto 1/app_server/controllers/registro.js
@@ -2,3 +2,8 @@
 module.exports.ingreso = function(req, res){
    res.render('ingreso', { title: 'Pagina de ingreso' });
 };
+
+/* Pagina de crear usuario */
+module.exports.creacion = function(req, res){
+   res.render('creacion', { title: 'Pagina creacion usuario' });
+};
--- Proyecto 1/app_server/routes/index.js
+++ Proyecto 1/app_server/routes/index.js
@@ -5,4 +5,6 @@ var ctrlRegistro = require('../controllers/registro');
 /* GET home page. */
 router.get('/', ctrlRegistro.ingreso); //pantalla de ingreso
 
+router.get('/creacion', ctrlRegistro.creacion); //pantalla de ingreso
+
 module.exports = router;

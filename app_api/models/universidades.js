af43906500737a49c33a30e9531a50bd07c1bc03
Meli Cordero <melisacorderoarias@hotmail.com>
2018-03-03 20:10:59 -0600

    Delete universidades.js

--- app_api/models/universidades.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-var universidadSchema = new mongoose.Schema({
-	nombre: {type:String, required:true}
-});
-
-mongoose.model('Universidad', universidadSchema, 'Universidad');
